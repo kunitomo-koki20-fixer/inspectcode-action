04a913f453510dc6045ab5ea8e948343d1e89d0b
kunitomo-koki20-fixer <kunitomo.koki20@fixer.co.jp>
2020-12-23 17:20:32 +0900

    🎨 edit building

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
-#!/bin/sh -l
+#!/bin/sh -l 
+for i in $5
+do
+    if [`echo $4 | grep $i`] ; then
+        list+=( $i )
+done
 
-dotnet build $1
+for j in list
+do
+    dotnet build ./$j/$j.csproj
+
+dotnet build $5
 /resharper/inspectcode.sh $1 $2 $3 $4 --no-swea
